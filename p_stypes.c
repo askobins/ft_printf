ae506ac3358a2440058c0165513f0a24eb6c5e06
askobins <askobins@student.42.fr>
2020-05-21 13:20:41 +0200

    fix typos

--- p_stypes.c
+++ p_stypes.c
@@ -6,7 +6,7 @@
 /*   By: askobins <askobins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/21 15:54:26 by askobins          #+#    #+#             */
-/*   Updated: 2020/05/21 13:09:12 by askobins         ###   ########.fr       */
+/*   Updated: 2020/05/21 13:20:25 by askobins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ size_t	p_char(char c, t_uchar flags, size_t w)
 		h_align(w - 1, ' ');
 	write(1, &c, 1);
 	if (flags & (1 << LFT) && w)
-		align (w - 1, ' ');
+		h_align (w - 1, ' ');
 	return (w ? w : 1);
 }
 
@@ -30,6 +30,6 @@ size_t	p_string(const char *str, t_uchar flags, size_t *wp)
 		h_align(wp[0], ' ');
 	write(1, str, wp[1]);
 	if (flags & (1 << LFT))
-		h_align(wp[0]' ');
+		h_align(wp[0], ' ');
 	return (wp[0] + wp[1]);
 }
