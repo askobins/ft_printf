a7ac73fb8ea398d2cebf11d577b365847bc4bd27
askobins <askobins@student.42.fr>
2020-07-17 15:47:26 +0200

    fix floats

--- src/p_ftypes.c
+++ src/p_ftypes.c
@@ -6,7 +6,7 @@
 /*   By: askobins <askobins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 20:00:17 by askobins          #+#    #+#             */
-/*   Updated: 2020/07/16 01:32:41 by askobins         ###   ########.fr       */
+/*   Updated: 2020/07/17 15:45:57 by askobins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ static size_t	p_float_normal(double nb, t_uint *wp)
 	u.raw.sign = 0;
 	exp = ft_abs((long)((int)u.raw.expo - 1023) * LOG) + (u.nb < 1);
 	exp += (t_uint)h_muldiv(u.nb, exp, u.nb < 1) > 9;
-	wp[0] = h_usub(wp[0], exp + !!sign + wp[1] + (wp[1] || g_flags.alt));
+	wp[0] = h_usub(wp[0], exp + !!sign + wp[1] + 2 - g_flags.alt);
 	if (!g_flags.lft && !g_flags.zro)
 		h_align(wp[0], ' ');
 	if (sign)
@@ -54,7 +54,7 @@ static size_t	p_float_normal(double nb, t_uint *wp)
 	if (!g_flags.lft && g_flags.zro)
 		h_align(wp[0], '0');
 	wp[1] = ft_putfloat(u.nb, exp, wp[1]);
-	if (g_flags.lft)
+	if (g_flags.lft && !g_flags.alt)
 		h_align(wp[0], ' ');
 	return (wp[0] + wp[1] + !!sign);
 }
@@ -78,6 +78,8 @@ static size_t	p_float_scient(double nb, t_uint *wp)
 	if (exp < 10)
 		write(1, "0", 1);
 	ft_putlong(exp, 10);
+	if (g_flags.lft)
+		h_align(wp[0], ' ');
 	return (len + 2 + h_numlen(exp, 10) + (exp < 9));
 }
 
@@ -101,9 +103,10 @@ size_t			p_float(double nb, t_uint *wp)
 		}
 		else
 		{
+			g_flags.alt = 1;
 			wp[1] = wp[1] ? wp[1] - 1 : 0;
 			return (p_float_scient(nb, wp));
 		}
 	}
-	return (g_flags.pre ? p_float_scient(nb, wp) : p_float_normal(nb, wp));
+	return (g_flags.alt ? p_float_scient(nb, wp) : p_float_normal(nb, wp));
 }
